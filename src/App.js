ad181d35f3db371e13f34d7dab10861f79afe000
hiroshi <hiroshi3110@gmail.com>
2017-10-22 21:29:34 +0900

    rooms.

--- src/App.js
+++ src/App.js
@@ -10,12 +10,69 @@ firebase.initializeApp({
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID
 });
 
+class Rooms extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {rooms: []}
+    let db = firebase.firestore();
+    db.collection("rooms").orderBy('timestamp').onSnapshot((querySnapshot) => {
+      let rooms = [];
+      querySnapshot.forEach((docSnap) => {
+        rooms.unshift(Object.assign({id: docSnap.id}, docSnap.data()));
+      });
+      this.setState({rooms: rooms})
+    });
+  }
+
+  createRoom = () => {
+    let db = firebase.firestore();
+    let name = this.input.value;
+    db.collection("rooms").add({
+      timestamp: Date(),
+      name: name
+    })
+    .then(function(docRef) {
+      console.log("Document written with ID: ", docRef.id);
+      let room = {id: docRef.id, name: name};
+      this.setState({currentRoom: room});
+    }.bind(this))
+    .catch(function(error) {
+      console.error("Error adding document: ", error);
+    });
+    this.input.value = ''
+  }
+
+  selectRoom = (room) => {
+    this.setState({currentRoom: room})
+  }
+
+  render () {
+    return (
+      <div>
+        <h3>Rooms</h3>
+        <div>
+          <input type='text' ref={(x) => {this.input = x}} />
+          <button onClick={this.createRoom}>Create Room</button>
+        </div>
+        <ul>
+          {
+            this.state.rooms.map((room) => {
+              return <li key={room.id}><a href="#" onClick={() => this.selectRoom(room)}>{room.name}</a></li>
+            })
+          }
+        </ul>
+        { this.state.currentRoom && <Messages room={this.state.currentRoom} /> }
+      </div>
+    );
+  }
+}
+
 class Messages extends Component {
   constructor (props) {
     super(props)
     this.state = {messages: []}
     let db = firebase.firestore();
-    db.collection("messages").orderBy('timestamp').onSnapshot((querySnapshot) => {
+    db.collection("messages").where('roomId', '==', props.room.id).orderBy('timestamp').onSnapshot((querySnapshot) => {
       let messages = [];
       querySnapshot.forEach((docSnap) => {
         messages.unshift(Object.assign({id: docSnap.id}, docSnap.data()));
@@ -28,22 +85,25 @@ class Messages extends Component {
     // add data
     let db = firebase.firestore();
     db.collection("messages").add({
+      roomId: this.props.room.id,
       timestamp: Date(),
       content: this.textarea.value
     })
     .then(function(docRef) {
-        console.log("Document written with ID: ", docRef.id);
+      console.log("Document written with ID: ", docRef.id);
     })
     .catch(function(error) {
-        console.error("Error adding document: ", error);
+      console.error("Error adding document: ", error);
     });
+    this.textarea.value = ''
   }
 
   render () {
     return (
       <div>
+        <h4>Messages</h4>
         <div>
-          <textarea ref={(x)=>{this.textarea = x}}></textarea>
+          <textarea ref={(x) => {this.textarea = x}}></textarea>
           <button onClick={this.push}>push</button>
         </div>
         <ul>
@@ -84,14 +144,14 @@ class App extends Component {
   }
 
   render () {
+    if (!this.state.user) {
+      return <button onClick={this.login}>login</button>
+    }
+
     return (
       <div>
-        {
-          this.state.user
-            ? this.state.user.email
-            : <button onClick={this.login}>login</button>
-        }
-        { this.state.user && <Messages /> }
+        <h2>{ this.state.user.email }</h2>
+        <Rooms />
       </div>
     );
   }
