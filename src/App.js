7d972bf20be78450d9b2de982258d90600fede39
hiroshi <hiroshi3110@gmail.com>
2017-10-24 22:27:30 +0900

    room.users

--- src/App.js
+++ src/App.js
@@ -10,58 +10,35 @@ firebase.initializeApp({
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID
 });
 
-class Rooms extends Component {
+class Members extends Component {
   constructor (props) {
-    super(props)
-    this.state = {rooms: []}
-    let db = firebase.firestore();
-    db.collection("rooms").orderBy('timestamp').onSnapshot((querySnapshot) => {
-      let rooms = [];
-      querySnapshot.forEach((docSnap) => {
-        rooms.unshift(Object.assign({id: docSnap.id}, docSnap.data()));
-      });
-      this.setState({rooms: rooms})
-    });
-  }
-
-  createRoom = () => {
-    let db = firebase.firestore();
-    let name = this.input.value;
-    db.collection("rooms").add({
-      timestamp: Date(),
-      name: name
-    })
-    .then(function(docRef) {
-      console.log("Document written with ID: ", docRef.id);
-      let room = {id: docRef.id, name: name};
-      this.setState({currentRoom: room});
-    }.bind(this))
-    .catch(function(error) {
-      console.error("Error adding document: ", error);
-    });
-    this.input.value = ''
-  }
-
-  selectRoom = (room) => {
-    this.setState({currentRoom: room})
+    super(props);
+    this.state = {users: []};
+    for (let uid in props.room.users) {
+      let db = firebase.firestore();
+      db.collection('users').doc(uid).get()
+        .then((docSnap) => {
+          let users = this.state.users;
+          users.push(Object.assign({id: uid}, docSnap.data()));
+          this.setState({users: users});
+        })
+        .catch((error) => {
+          console.error("Error getting document: ", error);
+        })
+    }
   }
 
   render () {
     return (
       <div>
-        <h3>Rooms</h3>
-        <div>
-          <input type='text' ref={(x) => {this.input = x}} />
-          <button onClick={this.createRoom}>Create Room</button>
-        </div>
+        <h4>Members</h4>
         <ul>
           {
-            this.state.rooms.map((room) => {
-              return <li key={room.id}><a href="#" onClick={() => this.selectRoom(room)}>{room.name}</a></li>
+            this.state.users.map((user) => {
+              return <li key={user.id}>{user.name}</li>
             })
           }
         </ul>
-        { this.state.currentRoom && <Messages room={this.state.currentRoom} /> }
       </div>
     );
   }
@@ -118,13 +95,88 @@ class Messages extends Component {
   }
 }
 
+class Rooms extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {rooms: []}
+    let db = firebase.firestore();
+    db.collection("rooms").orderBy('timestamp').onSnapshot((querySnapshot) => {
+      let rooms = [];
+      querySnapshot.forEach((docSnap) => {
+        rooms.unshift(Object.assign({id: docSnap.id}, docSnap.data()));
+      });
+      this.setState({rooms: rooms})
+    });
+  }
+
+  createRoom = () => {
+    let db = firebase.firestore();
+    let name = this.input.value;
+    let users = {};
+    users[this.props.user.id] = true;
+    db.collection("rooms").add({
+      timestamp: Date(),
+      name: name,
+      users: users
+    })
+    .then(function(docRef) {
+      console.log("Document written with ID: ", docRef.id);
+      let room = {id: docRef.id, name: name};
+      this.setState({currentRoom: room});
+    }.bind(this))
+    .catch(function(error) {
+      console.error("Error adding document: ", error);
+    });
+    this.input.value = ''
+  }
+
+  selectRoom = (room) => {
+    this.setState({currentRoom: room})
+  }
+
+  renderRoom = (room) => {
+    return (
+      <div>
+        <Members room={this.state.currentRoom} />
+        <Messages room={this.state.currentRoom} />
+      </div>
+    );
+  }
+
+  render () {
+    return (
+      <div>
+        <h3>Rooms</h3>
+        <div>
+          <input type='text' ref={(x) => {this.input = x}} />
+          <button onClick={this.createRoom}>Create Room</button>
+        </div>
+        <ul>
+          {
+            this.state.rooms.map((room) => {
+              return <li key={room.id}><a href="#" onClick={() => this.selectRoom(room)}>{room.name}</a></li>
+            })
+          }
+        </ul>
+        { this.state.currentRoom && this.renderRoom(this.state.currentRoom) }
+      </div>
+    );
+  }
+}
+
 class App extends Component {
   constructor (props) {
     super(props);
     this.state = {}
-    firebase.auth().onAuthStateChanged(function(user) {
-      if (user) {
-        this.setState({ user: user });
+    firebase.auth().onAuthStateChanged(function(authUser) {
+      if (authUser) {
+        let db = firebase.firestore();
+        let data = {
+          name: authUser.displayName,
+          email: authUser.email
+        };
+        db.collection('users').doc(authUser.uid).set(data, {merge: true});
+        this.setState({ user: Object.assign({id: authUser.uid}, data)});
       } else {
         console.log('no login')
       }
@@ -151,7 +203,7 @@ class App extends Component {
     return (
       <div>
         <h2>{ this.state.user.email }</h2>
-        <Rooms />
+        <Rooms user={this.state.user}/>
       </div>
     );
   }
